6b214d9470de913c2b8caf1393954d442b5793b0
binginx <sdaulibin@126.com>
2022-12-15 17:34:21 +0800

    feat: task add urfave cli test

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -8,31 +8,37 @@ import (
 )
 
 func main() {
-	app := cli.NewApp()
+	var language string
 
-	app.Flags = []cli.Flag{
-		&cli.StringFlag{
-			Name:  "lang",
-			Value: "english",
-			Usage: "language for the greeting",
+	app := &cli.App{
+		Name:    "brush",                // cli name
+		Version: "v1",                   // cli version
+		Usage:   "urfave cli test test", // usage
+		Flags: []cli.Flag{ // 接受的 flag
+			&cli.StringFlag{ // string
+				Name:        "lang",        // flag 名称
+				Aliases:     []string{"l"}, // 别名
+				Value:       "english",     // 默认值
+				Usage:       "language for the greeting",
+				Destination: &language, // 指定地址，如果没有可以通过 *cli.Context 的 GetString 获取
+				Required:    true,      // flag 必须设置
+			},
+		},
+		Action: func(c *cli.Context) error {
+			name := "who"
+			if c.NArg() > 0 {
+				name = c.Args().Get(0)
+			}
+			if language == "chinese" {
+				fmt.Println("你好啊", name)
+			} else {
+				fmt.Println("Hello", name)
+			}
+			return nil
 		},
 	}
 
-	app.Action = func(c *cli.Context) error {
-		name := "Nefertiti"
-		if c.NArg() > 0 {
-			name = c.Args().Get(0)
-		}
-		if c.String("lang") == "spanish" {
-			fmt.Println("Hola", name)
-		} else {
-			fmt.Println("Hello", name)
-		}
-		return nil
-	}
-
-	err := app.Run(os.Args)
-	if err != nil {
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
