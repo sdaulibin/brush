9dac89de5cd540511004c1339231c6dbcb379058
binginx <sdaulibin@126.com>
2022-12-16 17:25:49 +0800

    feat: task add urfave cli test

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -1,44 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"binginx.com/brush/cmd/api"
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
 )
 
 func main() {
-	var language string
-
-	app := &cli.App{
-		Name:    "brush",                // cli name
-		Version: "v1",                   // cli version
-		Usage:   "urfave cli test test", // usage
-		Flags: []cli.Flag{ // 接受的 flag
-			&cli.StringFlag{ // string
-				Name:        "lang",        // flag 名称
-				Aliases:     []string{"l"}, // 别名
-				Value:       "english",     // 默认值
-				Usage:       "language for the greeting",
-				Destination: &language, // 指定地址，如果没有可以通过 *cli.Context 的 GetString 获取
-				Required:    true,      // flag 必须设置
+	app := cli.NewApp()
+	app.Commands = []*cli.Command{
+		{
+			Name:    "server",
+			Aliases: []string{"s"},
+			Usage:   "启动brush",
+			Action: func(c *cli.Context) error {
+				api.Run()
+				return nil
 			},
 		},
-		Action: func(c *cli.Context) error {
-			name := "who"
-			if c.NArg() > 0 {
-				name = c.Args().Get(0)
-			}
-			if language == "chinese" {
-				fmt.Println("你好啊", name)
-			} else {
-				fmt.Println("Hello", name)
-			}
-			return nil
-		},
 	}
-
-	if err := app.Run(os.Args); err != nil {
+	err := app.Run(os.Args)
+	if err != nil {
 		log.Fatal(err)
 	}
 }
